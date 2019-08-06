fd307b1ad9fd5587eccb2b4820b9b8f435774bcf
Tranthanhhai281 <53221090+Tranthanhhai281@users.noreply.github.com>
2019-08-06 23:11:53 +0700

    bai tap ve nha

--- bai tap ve nha 10.cpp
+++ bai tap ve nha 10.cpp
@@ -1,18 +1,5 @@
 #include<stdio.h>
 
-bool find(int a[] , int n , int value)
-{
-	for(int i=0 ; i < n ; i++)
-	{
-		if(a[i] == value)
-		{
-			return true;
-		}
-	}
-	return false;
-}
-
-
 
 int main()
 {
@@ -26,15 +13,18 @@ int main()
 	{
 		printf("\n nhap cac phan tu:") ;
 		scanf("%d", &a[i]) ;
-		
-		
-		if(find(a, i, value) == true)
+		int j = 0 ;
+		while(j < i)
 		{
-	     	printf("\n Gia tri bi trung vui ong nhap lai") ;
-	     	i-- ;
+			if(a[j]==a[i])
+			{
+			  printf("\n Phan tu trung nhau!vui long nhap lai:") ;
+			  scanf("%d", &a[i]) ;
+			  j= 0 ;
+		    }
+			else
+			  j++ ;
 		}
-		else
-		a[i] = value ;
 	}
 	
 	return 0 ;
